996be72b3929683712f5e950ce9782f4865b62ae
Umang Kathuria <umang.kathuria-acn@hpe.com>
2020-04-24 00:28:29 +0530

    Code backup

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-module.exports.PATH_GET_ALL_APPS = "/getAllApps";
-module.exports.PATH_GET_APP_BY_ID = "/getAppById";
-module.exports.PATH_UPDATE_DATA = "/updateData";
+module.exports.PATH_GET_ALL_APPS = '/getAllApps';
+module.exports.PATH_GET_APP_BY_ID = '/getAppById';
+module.exports.PATH_UPDATE_DATA = '/updateData';
